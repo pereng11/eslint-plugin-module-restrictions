1b38617939d97ea52c535b561917a81466001611
Ellis <nardi0302@gmail.com>
2025-08-17 23:38:14 +0900

    feat: no-deep-import rule 추가
    - index파일이 존재할 경우, 외부에서 해당 디렉토리의 모듈을 참조할 때는 무조건 index를 참조하도록 제한
    - index파일 내부 모듈끼리 참조하는 경우는 별도 룰로 적절하게 제한해야 함

---
+++ src/shared/util.ts
@@ -0,0 +1,25 @@
+import fs from "fs";
+import * as path from "path";
+
+import { INDEX_PATTERNS } from "./const";
+
+export const findClosestIndexFile = (filePath: string) => {
+  let currentDir = filePath;
+
+  // 루트 디렉토리에 도달할 때까지 상위로 올라가면서 확인
+  while (currentDir !== path.dirname(currentDir)) {
+    // 현재 디렉토리에 index 파일이 있는지 확인
+    const hasIndex = INDEX_PATTERNS.some((pattern) => {
+      const indexPath = path.join(currentDir, pattern);
+      return fs.existsSync(indexPath);
+    });
+
+    if (hasIndex) {
+      return currentDir;
+    }
+
+    currentDir = path.dirname(currentDir);
+  }
+
+  return null;
+};
